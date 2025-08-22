7e50ad8581e52c0060f98804a59f4a89a5aebb80
bardiabsd <135510037+bardiabsd@users.noreply.github.com>
2025-08-22 23:22:25 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
-import telebot
 from flask import Flask, request
+import telebot
 
-TOKEN = "8339013760:AAEgr1PBFX59xc4cfTN2fWinWJHJUGWivdo"
-bot = telebot.TeleBot(TOKEN)
+API_TOKEN = "8339013760:AAEgr1PBFX59xc4cfTN2fWinWJHJUGWivdo"
+bot = telebot.TeleBot(API_TOKEN)
 app = Flask(__name__)
 
-# وقتی کاربر /start بزند
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "سلام! 👋 ربات روشنه ✅")
-
-# وبهوک
-@app.route('/webhook', methods=['POST'])
+@app.route("/webhook", methods=["POST"])
 def webhook():
-    update = request.get_json()
-    if update:
-        print(update)  # لاگ برای تست
-        bot.process_new_updates([telebot.types.Update.de_json(update)])
-    return "OK", 200
+    json_str = request.get_data().decode("UTF-8")
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "ok", 200
 
-@app.route('/')
-def index():
-    return "ربات تلگرام فعال است ✅", 200
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    bot.reply_to(message, "سلام! ربات فعاله ✅")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
